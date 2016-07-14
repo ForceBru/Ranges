b7af58989bb3b5ee7f140b848a5eaa8d343dd96e
ForceBru <ForceBru@users.noreply.github.com>
2016-07-14 23:21:18 +0400

    Added steps

--- Ranges.hpp
+++ Ranges.hpp
@@ -1,70 +1,109 @@
 //
 //  MyRange.hpp
-//  
+//
 //  Created by ForceBru on 13.07.16.
 //  Copyright © 2016 ForceBru. All rights reserved.
 //
 
-#ifndef MyRange_h
-#define MyRange_h
+#ifndef Ranges_hpp
+#define Ranges_hpp
 
-namespace Range {
+namespace Ranges {
 #include <type_traits>
-
+    template<typename Integer>
     class __Range;
     
     class __Number {
         __Number(const long long& a) { this->nmb = a; }
         
-        friend __Range operator, (const __Number& a, const __Number& b);
+        friend __Range<signed int> operator, (const __Number& a, const signed int& b);
+        friend __Range<signed long> operator, (const __Number& a, const signed long& b);
+        friend __Range<signed long long> operator, (const __Number& a, const signed long long& b);
+        
+        friend __Range<unsigned int> operator, (const __Number& a, const unsigned int& b);
+        friend __Range<unsigned long> operator, (const __Number& a, const unsigned long& b);
+        friend __Range<const unsigned long long> operator, (const __Number& a, const unsigned long long& b);
+        
+        template<typename Int>
+        friend __Range<signed int> operator, (const __Range<Int>& a, const signed int& b);
+        template<typename Int>
+        friend __Range<signed long> operator, (const __Range<Int>& a, const signed long& b);
+        template<typename Int>
+        friend __Range<signed long long> operator, (const __Range<Int>& a, const signed long long& b);
+        
+        template<typename Int>
+        friend __Range<unsigned int> operator, (const __Range<Int>& a, const unsigned int& b);
+        template<typename Int>
+        friend __Range<unsigned long> operator, (const __Range<Int>& a, const unsigned long& b);
+        template<typename Int>
+        friend __Range<const unsigned long long> operator, (const __Range<Int>& a, const unsigned long long& b);
+        
         friend __Number operator "" _r(unsigned long long k);
+        
+        friend class __Iter;
         
         long long nmb;
         
     public:
+        
+        template <typename Integer, typename = std::enable_if<std::is_integral<Integer>::value>>
+        operator Integer() const {
+            return Integer(nmb);
+        }
+        
         __Number operator- () { return __Number(-nmb); }
         const __Number operator- () const { return __Number(-nmb); }
     };
     
     class __Iter {
-        __Iter(const long long& a) { this->data = a; }
+        __Iter(const long long& a): data(__Number(a)) {}
         
         void fwd(const bool f) { this->forward = f; }
+        void stp(const long long& s) { this->step = s; }
         
-        long long data;
+        __Number data;
         bool forward;
-        
+        long long step;
+
+        template<typename Integer>
         friend class __Range;
         
     public:
-
-        template <typename Integer, typename = std::enable_if<std::is_integral<Integer>::value>>
-        inline Integer& operator* () { return Integer(this->data); }
-        inline bool operator!= (const __Iter rhs) { return this->data != rhs.data; }
         
-        template <typename Integer, typename = std::enable_if<std::is_integral<Integer>::value>>
-        inline const Integer& operator* () const { return Integer(this->data); }
-        inline const bool operator!= (const __Iter rhs) const { return this->data != rhs.data; }
+        inline __Number& operator* () { return this->data; }
+        inline bool operator!= (const __Iter rhs) {
+            if (forward) return this->data.nmb < rhs.data.nmb;
+            else return this->data.nmb > rhs.data.nmb;
+        }
+        
+        inline const __Number& operator* () const { return this->data; }
+        inline const bool operator!= (const __Iter rhs) const {
+            if (forward) return this->data.nmb < rhs.data.nmb;
+            else return this->data.nmb > rhs.data.nmb;
+        }
         
         inline __Iter operator++ () {
-            if (forward) ++this->data;
-            else         --this->data;
+            if (forward) this->data.nmb += step;
+            else         this->data.nmb -= step;
             return *this;
         }
     };
     
+    template<typename Integer>
     class __Range {
-        __Range(const long long& a, const long long& b): forward(1), st(a), en(b) {
+        __Range(const long long& a, const long long& b, const long long& step = 1): st(a), en(b) {
+            bool forward = 1;
             if (a > b) forward = 0;
             this->st.fwd(forward), this->en.fwd(forward);
+            this->st.stp(step), this->en.stp(step);
         }
         
-        friend __Range operator, (const __Number& a, const __Number& b) {
-            return __Range(a.nmb, b.nmb);
-        }
+        friend __Range operator, (const __Number& a, const Integer& b) { return __Range(a.nmb, b); }
+        
+        template<typename Int>
+        friend __Range operator, (const __Range<Int>& a, const Integer& stp) { return __Range(*(a.st), *(a.en), stp); }
         
         __Iter st, en;
-        bool forward;
         
     public:
         inline __Iter begin() { return this->st;}
@@ -74,10 +113,13 @@ namespace Range {
         inline const __Iter end() const { return this->en; }
     };
     
-    __Number operator "" _r(unsigned long long k) {
+    __Number operator "" _r(const unsigned long long k) {
         return __Number(k);
     }
-    
+
+    template<typename integer>
+    using Range = __Range<integer>;
 }
 
-#endif /* MyRange_h */
+
+#endif /* Ranges_hpp */
--- Ranges_test.cpp
+++ Ranges_test.cpp
@@ -1,5 +1,5 @@
 //
-//  MyRange.cpp
+//  Ranges_test.cpp
 //
 //  Created by ForceBru on 13.07.16.
 //  Copyright © 2016 ForceBru. All rights reserved.
@@ -8,91 +8,149 @@
 #include <iostream>
 #include <typeinfo>
 #include <vector>
+#include <chrono>
+#include <cassert>
 
 #include "Ranges.hpp"
 
 using namespace std;
-using namespace Range;
+using namespace Ranges;
 
 template<typename T>
 ostream& operator<<(ostream& out, const std::vector<T> data) {
     for(const T& a: data)
-        out << a;
+        out << a << ' ';
     
     return out;
 }
 
+#define TEST_IT { \
+    if (ret != correct) { \
+        cout << "[" << tests << "] ERROR" << endl; \
+        cout << "\tGot: " << ret << endl; \
+        cout << "\tCorrect: " << correct << endl; \
+        ++fail;\
+    } else cout << "[" << tests << "] OK" << endl, ++success; \
+}
+
+#define SETUP(type) vector<type> ret; cout << '\t';
 
 int main () {
-    cout << "1. Testing with smaller (unsigned) to greater (unsigned)..." << endl;
+    unsigned tests = 0, success = 0, fail = 0;
+    
+    
+    cout << ++tests << ". Testing with smaller (unsigned) to greater (unsigned)..." << endl;
     {
         const vector<int> correct = {0,1,2,3,4,5,6,7,8};
-        vector<int> ret;
-        cout << '\t';
-        for (const auto& i: 0_r, 9_r) {
+        SETUP(int);
+        for (const int& i: 0_r, 9) {
             cout << i << ' ';
             ret.push_back(i);
         }
         cout << endl;
         
-        if (ret != correct) {
-            cout << "[1] ERROR" << endl;
-            cout << "\tGot: " << ret << endl;
-            cout << "\tCorrect: " << correct << endl;
-        } else cout << "[1] OK" << endl;
+        TEST_IT;
     }
     
-    cout << "\n2. Testing with smaller (signed) to greater (unsigned)..." << endl;
+    cout << "\n" << ++tests << ". Testing with smaller (signed) to greater (unsigned)..." << endl;
     {
-        const vector<int> correct = {-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8};
-        vector<int> ret;
-        cout << '\t';
-        for (const auto& i: -5_r, 9_r) {
+        const vector<long> correct = {-9,-8,-7,-6,-5,-4,-3,-2,-1};
+        SETUP(long);
+        for (const long& i: -9_r, 0) {
             cout << i << ' ';
             ret.push_back(i);
         }
         cout << endl;
         
-        if (ret != correct) {
-            cout << "[2] ERROR" << endl;
-            cout << "\tGot: " << ret << endl;
-            cout << "\tCorrect: " << correct << endl;
-        } else cout << "[2] OK" << endl;
+        TEST_IT;
     }
     
-    cout << "\n3. Testing with equal values..." << endl;
+    cout << "\n" << ++tests << ". Testing with equal values..." << endl;
     {
-        const vector<int> correct = {};
-        vector<int> ret;
-        cout << '\t';
-        for (const auto& i: 0_r, 0_r) {
+        const vector<unsigned> correct = {};
+        SETUP(unsigned);
+        for (const unsigned& i: 0_r, 0) {
             cout << i << ' ';
             ret.push_back(i);
         }
         cout << endl;
         
-        if (ret != correct) {
-            cout << "[3] ERROR" << endl;
-            cout << "\tGot: " << ret << endl;
-            cout << "\tCorrect: " << correct << endl;
-        } else cout << "[3] OK" << endl;
+        TEST_IT;
     }
     
-    cout << "\n4. Testing with greater(unsigned) to smaller(unsigned)..." << endl;
+    cout << "\n" << ++tests << ". Testing with greater(unsigned) to smaller(unsigned)..." << endl;
     {
         const vector<int> correct = {9,8,7,6,5,4,3,2,1};
-        vector<int> ret;
-        cout << '\t';
-        for (const auto& i: 9_r, 0_r) {
+        SETUP(int);
+        for (const int& i: 9_r, 0) {
             cout << i << ' ';
             ret.push_back(i);
         }
         cout << endl;
         
-        if (ret != correct) {
-            cout << "[4] ERROR" << endl;
-            cout << "\tGot: " << ret << endl;
-            cout << "\tCorrect: " << correct << endl;
-        } else cout << "[4] OK" << endl;
+        TEST_IT;
     }
+    
+    cout << "\n" << ++tests << ". Testing with variables instead of literals..." << endl;
+    {
+        auto a = 9_r;
+        int b = 0;
+        const vector<int> correct = {9,8,7,6,5,4,3,2,1};
+        SETUP(int);
+        for (const int& i: a, b) {
+            cout << i << ' ';
+            ret.push_back(i);
+        }
+        cout << endl;
+        
+        TEST_IT;
+    }
+    
+    cout << "\n" << ++tests << ". Testing with steps (backward)..." << endl;
+    {
+        const vector<int> correct = {9,7,5,3,1};
+        SETUP(int);
+        for (const int& i: 9_r, 0, 2) {
+            cout << i << ' ';
+            ret.push_back(i);
+        }
+        cout << endl;
+        
+        TEST_IT;
+    }
+    
+    cout << "\n" << ++tests << ". Testing with steps (forward)..." << endl;
+    {
+        const vector<int> correct = {0,2,4,6,8};
+        SETUP(int);
+        for (const int& i: 0_r, 9, 2) {
+            cout << i << ' ';
+            ret.push_back(i);
+        }
+        cout << endl;
+        
+        TEST_IT;
+    }
+    
+    cout << "\n\n\tTotal tests: " << tests << endl;
+    cout << "\tSucceeded: " << success;
+    if  (fail) cout << endl << "\tFailed: " << fail << endl;
+    else cout << " (100%)" << endl;
+    
+    cout << endl << string(40, '-') << endl;
+    
+    
+    cout << "Performance test" << endl;
+    
+    auto started = chrono::high_resolution_clock::now();
+    
+    Range<int> myRange = (-100000_r, 100000, 3);
+    
+    for (volatile int i: myRange) {
+        cout << i << ' ';
+    }
+    
+    cout << endl << endl << "Looping lasted for " << chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - started).count() << " milliseconds" << endl;
+    
+    cout << "Considering that the above test was simply measuring the speed of std::cout, looping over a range is lightning-fast" << endl;
 }
